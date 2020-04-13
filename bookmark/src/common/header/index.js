8d7c39c6fc71dd2542aec7d2c2e1003eb2745e68
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2020-04-13 12:39:48 -0700

    header component complete with react-redux, axios, thunk

--- bookmark/src/common/header/index.js
+++ bookmark/src/common/header/index.js
@@ -1,25 +1,138 @@
 import React, { Component } from 'react';
-import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style';
+import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store';
+import { 
+  HeaderWrapper, 
+  Logo, 
+  NavItem, 
+  NavSearch, 
+  SearchInfo,
+  Addition, 
+  Button, 
+  SearchWrapper,
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoItem,
+  SearchInfoList,
+  AaPic,
+  MagPic,
+  PenPic,
+  Nav
+} from './style';
 
 class Header extends Component {
+
+  getListArea() {
+    const { focused, list, page, mouseIn, totalPage, handleMouseEnter, handleMouseLeave, handleChangePage } = this.props;
+    const newList = list.toJS();
+    const pageList = [];
+
+    if (newList.length) {
+      for (let i = ((page - 1) * 10); i < page * 10; i++) {
+        pageList.push(
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+        )
+      }
+    }
+
+    if (focused || mouseIn) {
+      return (
+        <SearchInfo 
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <SearchInfoTitle>
+            Hot Search
+            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage)}>Refresh</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            { pageList }
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null;
+    }
+  }
+
   render() {
-    return ( 
+    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
+    return (
       <HeaderWrapper>
         <Logo />
         <Nav>
           <NavItem className='left active'>Main Page</NavItem>
           <NavItem className='left'>Download App</NavItem>
           <NavItem className='right'>Login</NavItem>
-          <NavItem className='right'>Aa</NavItem>
-          <NavSearch></NavSearch>
+          <NavItem className='right'><AaPic /></NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={focused ? 'focused': ''} 
+                onFocus={() => handleInputFocus(list)}
+                onBlur={handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={focused ? 'focused magPic': 'magPic'}>
+              <MagPic />
+            </i>
+            {this.getListArea()}
+          </SearchWrapper>
         </Nav>
         <Addition>
-          <Button className='writting'>Write Something</Button>
+          <Button className='writting'>
+            <i className='penPic'><PenPic /></i>
+            Write Somthing
+          </Button>
           <Button className='reg'>Register</Button>
         </Addition>
       </HeaderWrapper>
-    )
+    );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    focused: state.getIn(['header', 'focused']),
+    list: state.getIn(['header', 'list']),
+    page: state.getIn(['header', 'page']),
+    totalPage: state.getIn(['header', 'totalPage']),
+    mouseIn: state.getIn(['header', 'mouseIn'])
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
+      dispatch(actionCreators.searchFocus());
+    },
+
+    handleInputBlur() {
+      dispatch(actionCreators.searchBlur());
+    }, 
+
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter())
+    },
+
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave())
+    },
+
+    handleChangePage(page, totalPage) {
+      if (page < totalPage) {
+        dispatch(actionCreators.changePage(page + 1));
+      } else {
+        dispatch(actionCreators.changePage(1));
+      }
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- bookmark/src/common/header/style.js
+++ bookmark/src/common/header/style.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import logoPic from '../../statics/logo.png';
+import magPic from '../../statics/magnifier.svg';
+import aaPic from '../../statics/Aa.svg';
+import penPic from '../../statics/pen.svg';
 
 export const HeaderWrapper = styled.div`
   position: relative;
@@ -20,6 +23,48 @@ export const Logo = styled.a.attrs({
   background-size: contain;
 `;
 
+export const AaPic = styled.a.attrs({
+  href: '/'
+})`
+  position: relative;
+  top: 15px;
+  left: 0;
+  display: block;
+  width: 30px;
+  height: 28px;
+  background: url(${aaPic});
+  background-size: contain;
+`;
+
+export const MagPic = styled.a.attrs({
+  href: '/'
+})`
+  float: right;
+  top: 0;
+  left: 0;
+  margin-top: 5px;
+  margin-right: 5px;
+  display: block;
+  width: 20px;
+  height: 20px;
+  background: url(${magPic});
+  background-size: contain;
+  text-align: center;
+`;
+
+export const PenPic = styled.a.attrs({
+  href: '/'
+})`
+  position: relative;
+  top: 3px;
+  left: 0;
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  background: url(${penPic});
+  background-size: contain;
+`;
+
 export const Nav = styled.div`
   width: 1060px;
   height: 100%;
@@ -36,22 +81,79 @@ export const NavItem = styled.div`
 
   &.left {
     float: left;
+    color: #707070;
   }
   &.right {
     float: right;
-    color: #969696;
+    color: #707070;
   }
   &.active {
     color: #ea6f5a;
   }
 `
 
+export const SearchWrapper = styled.div`
+  position: relative;
+  float: left;
+  .magPic {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    width: 30px;
+    height: 30px;
+    border-radius: 13px;
+    &.focused {
+      background: white;
+    }
+  }
+`;
+
+export const SearchInfo = styled.div`
+  position: absolute;
+  left = 0;
+  top: 56px;
+  padding: 0 20px;
+  width: 360px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, .2);
+`;
+
+export const SearchInfoTitle = styled.div`
+  margin-top: 20px;
+  margin-bottom: 15px;
+  line-height: 20px;
+  font-size: 14px;
+  color: #969696;
+`;
+
+export const SearchInfoSwitch = styled.span`
+  float: right;
+  font-size: 13px;
+  cursor: pointer;
+`;
+
+export const SearchInfoItem = styled.a`
+  display: block;
+  float: left;
+  line-height: 20px;
+  padding: 0 5px;
+  margin-right: 10px;
+  margin-bottom: 15px;
+  font-size: 12px;
+  border: 1px solid #ddd;
+  color: #787878;
+  border-radius: 3px;
+`;
+
+export const SearchInfoList = styled.div`
+  overflow: hidden;
+`
+
 export const NavSearch = styled.input.attrs ({
   placeholder: 'search'
 })`
-  width: 160px;
+  width: 180px;
   height: 38px;
-  padding: 0 10px;
+  padding: 0 35px 0 15px;
   box-sizing: border-box;
   margin-top: 9px;
   margin-left: 20px;
@@ -60,10 +162,28 @@ export const NavSearch = styled.input.attrs ({
   border-radius: 13px;
   background: #eee;
   font-size: 15px;
+  color: #777;
   &::placeholder {
     color: #999;
   }
-`
+  &.focused {
+    width: 350px;
+  }
+  &.slide-enter {
+    width: 180px;
+    transition: all .2s ease-out;
+  }
+  &.slide-enter-active {
+    width: 350px;
+  }
+  &.slide-exit {
+    width: 350px;
+    transition: all .2s ease-out;
+  }
+  &.slide-exit-active {
+    width: 180px;
+  }
+`;
 
 export const Addition = styled.div`
   position: absolute;
@@ -88,4 +208,5 @@ export const Button = styled.div`
     color: #fff;
     background: #ec6149;
   }
+  
 `
